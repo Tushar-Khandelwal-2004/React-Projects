8746f246f6fb9ff273fd8e62470e6a4eaf6e6785
Tushar <khandelwal.tushar8527@gmail.com>
2025-01-09 00:00:22 +0530

    Add files via upload

---
+++ Todo App/backend/routes/user.js
@@ -0,0 +1,89 @@
+const { Router } = require("express");
+const { userModel } = require("../db");
+const { authToken } = require("../middlewares/authToken")
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userRouter = Router();
+const JWT_SECRET = process.env.JWT_USER;
+
+userRouter.post("/signup", async function (req, res) {
+    const requiredBody = z.object({
+        email: z.string().email(),
+        password: z.string().max(100).min(4),
+        firstname: z.string().min(2).max(200),
+        lastname: z.string().min(2).max(200)
+    })
+    const parsedBody = requiredBody.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.json({
+            message: "You must fill all details correctly",
+            success: false,
+            // error: parsedBody.error.errors
+        })
+    }
+
+    const { email, password, firstname, lastname } = req.body;
+
+    try {
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.json({
+                success: false,
+                message: "User already exists."
+            })
+        }
+        const hashedPassowrd = await bcrypt.hash(password, 5);
+        await userModel.create({
+            email: email,
+            password: hashedPassowrd,
+            firstname: firstname,
+            lastname: lastname
+        })
+        res.json({
+            success: true,
+            message: "You have successfully signed up!"
+        })
+    } catch (e) {
+        return res.json({
+            success: false,
+            message: "An error occurred while signing up"
+        });
+    }
+
+})
+userRouter.post("/signin", async function (req, res) {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+        return res.status(403).send({
+            success: false,
+            message: "User does not exist",
+
+        });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+        const token = jwt.sign({
+            id: user._id.toString(),
+        }, JWT_SECRET);
+        res.json({
+            success: true,
+            token: token,
+            message: "You have successfully signed in!"
+        })
+
+    }
+    else {
+        res.status(403).send({
+            success: false,
+            message: "Incorrect Credentials!"
+        })
+    }
+
+});
+
+module.exports = {
+    userRouter: userRouter
+}
